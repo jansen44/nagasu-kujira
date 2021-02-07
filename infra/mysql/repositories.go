ac2ef498d6550fdab55b2e6ff6f1a46fbab50f4f
jansen44 <jsd.alcantara@gmail.com>
2021-02-07 12:22:31 -0400

    feat: Added mysql migrations routine

--- infra/mysql/repositories.go
+++ infra/mysql/repositories.go
@@ -5,14 +5,23 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jansen44/nagasu-kujira/util"
+	migrate "github.com/rubenv/sql-migrate"
 	"github.com/sirupsen/logrus"
 )
 
 func InitRepositories(config *util.Config) error {
-	logrus.Info("## Initializing MySQL Repositories!")
+	logrus.Info("## Initializing MySQL Repositories...")
 	mysqlContext := context.Background()
-
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/nagasu?parseTime=true")
+	if err != nil {
+		return err
+	}
+
+	logrus.Info("## Handling MySQL migrations...")
+	migrations := &migrate.FileMigrationSource{
+		Dir: "infra/mysql/migrations",
+	}
+	_, err = migrate.Exec(db, "mysql", migrations, migrate.Up)
 	if err != nil {
 		return err
 	}
