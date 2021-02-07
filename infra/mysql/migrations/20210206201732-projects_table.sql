0cabd1ccfca45c9880aa98bde44a3e45987d75de
jansen44 <jsd.alcantara@gmail.com>
2021-02-07 01:23:44 -0400

    feat: Updated 20210206201732-projects_table.sql  - added created_at column  - added updated_at column

--- infra/mysql/migrations/20210206201732-projects_table.sql
+++ infra/mysql/migrations/20210206201732-projects_table.sql
@@ -2,6 +2,8 @@
 create table projects (
     id int auto_increment,
     name varchar(255) not null,
+    created_at timestamp default NOW() not null,
+    updated_at timestamp default NOW() not null,
     constraint projects_pk
         primary key (id)
 );
