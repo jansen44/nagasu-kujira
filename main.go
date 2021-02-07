d5ad04cf86632273fa7a0ff1d389f613e6b35c36
jansen44 <jsd.alcantara@gmail.com>
2021-02-07 02:21:02 -0400

    feat: base main structure + driver added to config.go

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"github.com/jansen44/nagasu-kujira/drivers/rest"
+	"github.com/jansen44/nagasu-kujira/infra/mysql"
+	"github.com/jansen44/nagasu-kujira/util"
 	"github.com/sirupsen/logrus"
 )
 
+func initRepositories(config *util.Config) error {
+	logrus.Info("## Initializing Repositories...")
+	switch config.DataSource {
+	case util.MySqlDataSource:
+		err := mysql.InitRepositories(config)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func initDrivers(config *util.Config) error {
+	logrus.Info("## Initializing Drivers...")
+	switch config.Driver {
+	case util.RestAPIDriver:
+		client := rest.NewRestAPIClient(config)
+		client.Start()
+	}
+	return nil
+}
+
 func main() {
-	logrus.Info("## Application Start!")
+	config := util.Config{DataSource: util.MySqlDataSource, Driver: util.RestAPIDriver}
+	err := initRepositories(&config)
+	if err != nil {
+		panic(err)
+	}
+	err = initDrivers(&config)
+	if err != nil {
+		panic(err)
+	}
 }
--- util/config.go
+++ util/config.go
@@ -3,13 +3,19 @@ package util
 import "github.com/jansen44/nagasu-kujira/core/repositories"
 
 type DataSources string
+type Drivers string
 
 const (
 	MySqlDataSource DataSources = "mysql"
 )
 
+const (
+	RestAPIDriver Drivers = "rest"
+)
+
 type Config struct {
 	DataSource DataSources
+	Driver     Drivers
 
 	// Repositories ==================
 	ProjectRepository repositories.IProjectsRepository
